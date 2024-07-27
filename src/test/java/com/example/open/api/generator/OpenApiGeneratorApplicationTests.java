425028a9648d7aa6818b6759345b1db8d7a21525
abdoulc <abdoulrazak1996@gmail.com>
2024-07-27 20:12:26 +0200

    Initial commit

---
+++ src/test/java/com/example/open/api/generator/OpenApiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.open.api.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
